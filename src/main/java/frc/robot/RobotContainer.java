fd62ec465a932e6f2a8dce58d02876f643e39507
Avery <averyg883@icloud.com>
2024-01-07 15:58:03 -0500

    Setup example auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 //import frc.robot.subsystems.SwerveDrivetrain;
@@ -11,7 +13,7 @@ public class RobotContainer {
     private final CommandXboxController operatorController = new CommandXboxController(1);
 
     public Command getAutonomousCommand() {
-        return null;
+        return new PathPlannerAuto("Test Auto");
     }
 
     private void configureBindings() {
