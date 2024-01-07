a4524f7c603cd145057213f53874fecbc424b8b3
[Error_27] <error27dev@gmail.com>
2024-01-07 16:03:47 -0500

    fix package statement

--- src/main/java/frc/robot/subsystems/PretendSubsystem.java
+++ src/main/java/frc/robot/subsystems/PretendSubsystem.java
@@ -1,4 +1,4 @@
-package frc.robot.commands;
+package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
