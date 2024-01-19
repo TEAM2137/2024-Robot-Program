513c8e3dafd30da3b44abd5338953c49e27b21e2
RedCoffeeMug <redcoffeemuggithub@gmail.com>
2024-01-18 19:27:34 -0500

    Intake PID

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -16,11 +18,31 @@ public class IntakeSubsystem extends SubsystemBase {
     private CANSparkMax pivotMotor;
     private CANSparkMax rollerMotor;
 
+    private SparkPIDController pivotPIDController;
+    private SparkPIDController rollerPIDController;
+
+    private RelativeEncoder pivotEncoder;
+
+
   
     public IntakeSubsystem() {
         super();
+
         pivotMotor = new CANSparkMax(pivotID, CANSparkLowLevel.MotorType.kBrushless);
+        pivotEncoder = pivotMotor.getEncoder();
+        pivotPIDController = pivotMotor.getPIDController();
+        pivotPIDController.setFeedbackDevice(pivotEncoder);
+        pivotPIDController.setP(0.1);
+        pivotPIDController.setI(0.2);
+        pivotPIDController.setD(0.3);
+        pivotPIDController.setFF(0.4);
+
         rollerMotor = new CANSparkMax(rollerID, CANSparkLowLevel.MotorType.kBrushless);
+        rollerPIDController = rollerMotor.getPIDController();
+        rollerPIDController.setP(0.01);
+        rollerPIDController.setI(0.02);
+        rollerPIDController.setD(0.03);
+        rollerPIDController.setFF(0.04);
     }
 
     
