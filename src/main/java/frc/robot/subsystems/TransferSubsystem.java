ac1938f75841282397a59e30495ed2fe4f567e4a
[Error_27] <error27dev@gmail.com>
2024-01-27 09:14:48 -0500

    Javadoc comments on transfer

--- src/main/java/frc/robot/subsystems/TransferSubsystem.java
+++ src/main/java/frc/robot/subsystems/TransferSubsystem.java
@@ -24,6 +24,11 @@ public class TransferSubsystem extends SubsystemBase {
         beltMotor = new CANSparkMax(CanIDs.get("transfer-motor"), CANSparkLowLevel.MotorType.kBrushless);
     }
 
+    /**
+     * Command to intake a NOTE into the transfer, which runs until a stop condition is met or a note is in the transfer
+     * @param earlyStop Condition to stop early
+     * @return The command
+     */
     public Command intakeCommand(BooleanSupplier earlyStop) {
         return runEnd(
             () -> beltMotor.set(.5),
@@ -34,10 +39,18 @@ public class TransferSubsystem extends SubsystemBase {
         ).until(() -> beamBreak.get() || earlyStop.getAsBoolean()); // Stop when the beam breaks
     }
 
+    /**
+     * Command to shut off the motor
+     * @return The command
+     */
     public Command shutoffCommand() {
         return runOnce(() -> beltMotor.stopMotor());
     }
 
+    /**
+     * Command to feed the NOTE into the shooter
+     * @return The command
+     */
     public Command feedCommand() {
         return runEnd(
             () -> beltMotor.set(.5),
@@ -48,6 +61,10 @@ public class TransferSubsystem extends SubsystemBase {
         ).withTimeout(.5); // Stop after a timeout
     }
 
+    /**
+     * Get whether the transfer has a NOTE in it
+     * @return whether the transfer is occupied
+     */
     public boolean getOccupied() {
         return occupied;
     }
