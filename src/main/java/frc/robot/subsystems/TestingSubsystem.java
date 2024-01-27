39b5c9d6b747d559bf436c1b04a2f366e223b649
[Error_27] <error27dev@gmail.com>
2024-01-27 11:35:53 -0500

    i made a typo

--- src/main/java/frc/robot/subsystems/TestingSubsystem.java
+++ src/main/java/frc/robot/subsystems/TestingSubsystem.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.util.CanIDs;
 
 public class TestingSubsystem extends SubsystemBase {
-    private CANSparkMax testMotor = new CANSparkMax(CanIDs.get("test-motor"), MotorType.kBrushless);
+    private CANSparkMax testMotor = new CANSparkMax(CanIDs.get("testing-motor"), MotorType.kBrushless);
 
     public Command testMotorOn() {
         return runOnce(() -> testMotor.set(.3));
