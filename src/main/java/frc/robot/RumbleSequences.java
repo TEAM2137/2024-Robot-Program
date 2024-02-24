94a36af1a166418bebbeba423f64e11e96159fb3
[Error_27] <error27dev@gmail.com>
2024-02-24 15:08:12 -0500

    add in some rumble sequences

---
+++ src/main/java/frc/robot/RumbleSequences.java
@@ -0,0 +1,30 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+public class RumbleSequences {
+    public static Command rumbleDualPulse(XboxController controller) {
+        return 
+            rumble(controller, RumbleType.kBothRumble, 1)
+            .andThen(CommandSequences.timingCommand(.1))
+            .andThen(rumble(controller, RumbleType.kBothRumble, 0))
+            .andThen(CommandSequences.timingCommand(.1))
+            .andThen(rumble(controller, RumbleType.kBothRumble, 1))
+            .andThen(CommandSequences.timingCommand(.1))
+            .andThen(rumble(controller, RumbleType.kBothRumble, 0));
+    }
+
+    public static Command rumbleOnce(XboxController controller) {
+        return
+            rumble(controller, RumbleType.kBothRumble, 1)
+            .andThen(CommandSequences.timingCommand(.2))
+            .andThen(rumble(controller, RumbleType.kBothRumble, 0));
+    }
+
+    public static Command rumble(XboxController controller, RumbleType rumble, double value) {
+        return new InstantCommand(() -> controller.setRumble(rumble, value));
+    }
+}
